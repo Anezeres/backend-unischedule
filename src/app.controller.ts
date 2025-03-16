4e66d90e00d9a6604f00dcc86a69c7da4b47ea7c
dniel <daniel.arias.castrillon@correounivalle.edu.com>
2025-03-15 23:03:32 -0500

    Se hace la conexión a la DB usando Mongo DB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MongoDBService } from './database/mongodb.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly mongoDBService: MongoDBService,
+  ) {}
 
   @Get()
   getHello(): string {
