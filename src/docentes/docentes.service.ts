54be1e7badc4342c76f5bbb24381206e44a83a0c
DeibyMP <79093431+DeibyMP@users.noreply.github.com>
2025-03-21 10:07:24 -0500

    first-version-crud-docentes

---
+++ src/docentes/docentes.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, isValidObjectId } from 'mongoose';
+import { Docente } from './schemas/docentes.schema';
+import { CreateDocentesDto } from './dto/create-docentes.dto';
+import { UpdateDocentesDto } from './dto/update-docentes.dto';
+
+@Injectable()
+export class DocentesService {
+  constructor(
+    @InjectModel(Docente.name) private docenteModel: Model<Docente>,
+  ) {}
+
+  async findAll(): Promise<Docente[]> {
+    return this.docenteModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Docente> {
+    if (!isValidObjectId(id)) {
+      throw new NotFoundException('ID inválido');
+    }
+  
+    const docente = await this.docenteModel.findById(id).exec();
+    if (!docente) throw new NotFoundException('Docente no encontrado');
+    return docente;
+  }
+
+  async create(createDocenteDto: CreateDocentesDto): Promise<Docente> {
+    const docente = new this.docenteModel(createDocenteDto);
+    return docente.save();
+  }
+
+  async update(
+    id: string,
+    updateDocenteDto: UpdateDocentesDto,
+  ): Promise<Docente> {
+    const docente = await this.docenteModel.findByIdAndUpdate(
+      id,
+      updateDocenteDto,
+      { new: true },
+    );
+    if (!docente) throw new NotFoundException('Docente no encontrado');
+    return docente;
+  }
+
+  async delete(id: string): Promise<void> {
+    const result = await this.docenteModel.findByIdAndDelete(id);
+    if (!result) throw new NotFoundException('Docente no encontrado');
+  }
+}
