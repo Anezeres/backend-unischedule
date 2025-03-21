ad2b7523c021a3c325e25a45990d3f7e4c4d43c1
DeibyMP <79093431+DeibyMP@users.noreply.github.com>
2025-03-21 10:08:27 -0500

    first-version-crud-docentes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }))
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API de Docentes') // Título de la API
+    .setDescription('Documentación de la API para el manejo de docentes') // Descripción
+    .setVersion('1.0') // Versión de la API
+    .addTag('docentes') // Etiqueta para agrupar los endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Ruta para acceder a la documentación: /api
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
