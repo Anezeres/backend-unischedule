8ed1b8ff41f2e63e068a50dbc90c08107eb11959
DeibyMP <79093431+DeibyMP@users.noreply.github.com>
2025-03-21 10:08:03 -0500

    first-version-crud-docentes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-
+import { DocentesModule } from './docentes/docentes.module';
 
 @Module({
-  imports: [UsersModule],
-
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/docentes'),
+    UsersModule,
+    DocentesModule,
+  ],
 })
 export class AppModule {}
